c3a802bbaf140eb1e82f1a1cd3144efa193b4995
ivyztech <108420656+ivyztech@users.noreply.github.com>
2024-08-21 20:42:56 +1000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 echo "Running unit tests..."
-# Simulate unit tests
-echo "All unit tests passed."
-
-echo "Running integration tests..."
-# Simulate integration tests
-echo "All integration tests passed."
+java MainTest
+echo "Unit tests completed."
